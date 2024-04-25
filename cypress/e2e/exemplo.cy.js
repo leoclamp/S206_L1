48ca0890f8eec7199a334273272163efa5092488
leofiore <leonardo.matias@gec.inatel.br>
2024-04-25 13:25:35 -0300

    :white_check_mark: test: adicionando alguns testes

--- cypress/e2e/exemplo.cy.js
+++ cypress/e2e/exemplo.cy.js
@@ -1,5 +1,65 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe("testes de criação, registro e login", () => {
+  it("teste criação de usuario com sucesso", () => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+    cy.get('.btn-link').click()
+    cy.get('#firstName').type("leonardo")
+    cy.get('#Text1').type("junior")
+    cy.get('#username').type("leo")
+    cy.get('#password').type("123")
+    cy.get('.btn-primary').click()
+    cy.get('.ng-binding').should("contain.text", "Registration successful")
   })
+
+  it("teste criação de usuario com sucesso", () => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+    cy.get('.btn-link').click()
+    cy.get('#firstName').type("leonardo")
+    cy.get('#Text1').type("junior")
+    cy.get('#username').type("leo")
+    cy.get('.btn-primary').should("be.disabled")
+  })
+
+  it("teste de login com sucesso", () => {
+    let infos = criarUser()
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+    cy.get('#username').type(infos[0])
+    cy.get('#password').type(infos[1])
+    cy.get('.btn-primary').click()
+    cy.get('h1.ng-binding').should("contain.text", infos[0])
+  })
+
+  it("teste de delete com sucesso", () => {
+    let infos = criarUser()
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+    cy.get('#username').type(infos[0])
+    cy.get('#password').type(infos[1])
+    cy.get('.btn-primary').click()
+    cy.get('h1.ng-binding').should("contain.text", infos[0])
+    cy.get('.ng-binding > a').click()
+    cy.get('.btn').click()
+    cy.get('#username').type(infos[0])
+    cy.get('#password').type(infos[1])
+    cy.get('.btn-primary').click()
+    cy.get('.ng-binding').should("contain.text", "Username or password is incorrect")
+  })
+
 })
+
+function criarUser(){
+  let hora = new Date().getHours().toString()
+  let minute = new Date().getMinutes().toString()
+  let seg = new Date().getSeconds().toString()
+  let id = hora + minute + seg + "id"
+  let senha = hora + minute + seg + "senha"
+  let infos = [id, senha]
+
+  cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+  cy.get('.btn-link').click()
+  cy.get('#firstName').type(id)
+  cy.get('#Text1').type(id)
+  cy.get('#username').type(id)
+  cy.get('#password').type(senha)
+  cy.get('.btn-primary').click()
+  cy.get('.ng-binding').should("contain.text", "Registration successful")
+  return infos
+}
